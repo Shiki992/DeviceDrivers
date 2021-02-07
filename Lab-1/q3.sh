75651209c2d3b52d533f5fa063a255bdb188773f
Hrishikesh Vedantam <shikiikihs2@gmail.com>
2021-02-07 17:27:12 +0400

    q3 updates

--- Lab-1/q3.sh
+++ Lab-1/q3.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-#---------------------------------------------------------
-# created by alvin alexander, http://alvinalexander.com
-#---------------------------------------------------------
-# released under the creative commons share-alike license:
-# http://creativecommons.org/licenses/by-sa/2.5/
-# link to this web page without the 'nofollow' tag if you
-# copy this work.
-#---------------------------------------------------------
 
 # the output file
 FILE=`pwd`/downloaded_data.out
@@ -16,16 +8,11 @@ FILE=`pwd`/downloaded_data.out
 URL=http://www.example.com/foo/bar
 
 # write header information to the log file
-start_date=`date`
-echo "START-------------------------------------------------" >> $FILE
 echo "" >> $FILE
-
 # retrieve the web page using curl. time the process with the time command.
-time (curl --connect-timeout 100 $URL) >> $FILE 2>
-
+for i in 1 2 3 4 5 6 7 8 9 10
+do
+	time(curl --connect-timeout 100 ($URL))>>$FILE;
+done
 # write additional footer information to the log file
-echo "" >> $FILE
-end_date=`date`
-echo "STARTTIME: $start_date" >> $FILE
-echo "END TIME:  $end_date" >> $FILE
 echo "" >> $FILE
